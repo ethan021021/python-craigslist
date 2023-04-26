2f8c2343bdf5ba8c5a1f149c26239617618443f5
Ethan Thomas <ethan021@me.com>
2023-04-25 17:58:51 -0700

    rever

--- craigslist/base.py
+++ craigslist/base.py
@@ -195,7 +195,9 @@ class CraigslistBase(object):
             # self.logger.info('GET %s', response.url)
             # self.logger.info('Response code: %s', response.status_code)
             # response.raise_for_status()  # Something failed?
-            self.driver.get("https://sfbay.craigslist.org/search/sfc/apa?min_bedrooms=1#search=1~gallery~0~0")
+            self.driver.get(self.url)
+
+            print(self.url)
 
             time.sleep(2)
 
